4ec5235c7c7ea45cdf19d762e46567d18b0baafe
naiv <nazarivankiv1@gmail.com>
2023-12-11 15:03:42 +0200

    updated admin panel

--- flight_service/admin.py
+++ flight_service/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from flight_service.models import (
+    Crew,
+    Airport,
+    Order,
+    AirplaneType,
+    Airplane,
+    Flight,
+    Ticket,
+    Route,
+)
+
+
+admin.site.register(Crew)
+admin.site.register(Airport)
+admin.site.register(Order)
+admin.site.register(AirplaneType)
+admin.site.register(Airplane)
+admin.site.register(Flight)
+admin.site.register(Ticket)
+admin.site.register(Route)
